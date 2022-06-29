16cb5db2c14e461643618da9fcf44c0cccf7667b
Faraja17 <farajakthompson@gmail.com>
2022-06-29 04:04:17 -0400

    Independent Work: added a variable to say 1 like, 2 likes
    used what I learned from 99 bottles of beer project to get the grammar correct. I am extremely proud of myself! I also solved a bug. the ternary operator was printing an extra like/likes. I put it into a div and used CSS to set the div's display: none; So HAPPY! I'm almost done with the project.

--- src/Components/Likes.js
+++ src/Components/Likes.js
@@ -1,26 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const Likes = (props) => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    console.log(`likes: ${count}`);
+  // console.log(`likes: ${count}`);
+  let counterWord = "likes";
 
-    return(
-        <>
-            
-            <div className='likes'>
-                <img src={props.post} alt='kitten pics'/>
-                <div className="likesCount">
-                    <h3 style={{ visibility: count === 0 ? 'hidden' : 'visible' }}>{count} likes</h3>
-                    <button className='like' type='submit' onClick= {() => setCount(count + 1)}>Like</button>
-                    {/* <button onClick ={() => setCount(0)}>reset</button>
+  return (
+    <>
+      <div className="likes">
+        <img src={props.post} alt="kitten pics" />
+        <div className="likesCount">
+          <div className="counterWord">
+            {count === 1 ? (counterWord = "like") : (counterWord = "likes")};
+          </div>
+          <h3 style={{ visibility: count === 0 ? "hidden" : "visible" }}>
+            {count} {counterWord}
+          </h3>
+          <button
+            className="like"
+            type="button"
+            onClick={() => setCount(count + 1)}
+          >
+            Like
+          </button>
+          {/* <button onClick ={() => setCount(0)}>reset</button>
                     <button className='like' type='submit' onClick= {() => setCount(count - 1)}>Disike</button> */}
-
-                </div>
-            </div>
-           
-        </>
-    );
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Likes;
